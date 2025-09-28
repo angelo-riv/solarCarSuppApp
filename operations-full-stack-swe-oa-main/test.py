4130a2f7b8e2f4d741453ab8d13ea820318f7f0a
angelo-riv <angelosimbolrivera@gmail.com>
2025-09-28 18:29:28 -0400

    finished solar car app

--- operations-full-stack-swe-oa-main/test.py
+++
@@ -1,9 +0,0 @@
-
-def sorted_numbers(numbers: list):
-    if numbers:
-        return "hi"
-    sorted_numbers = sorted(numbers)
-    return {"original_numbers":numbers,
-            "sorted_numbers":sorted_numbers}
-
-print(sorted_numbers([45, 23, 78, 12, 91, 34, 67, 89, 56, 3]))
